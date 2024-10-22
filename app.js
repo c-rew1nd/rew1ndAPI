8bd976e1ad6aff8923f1835b13bf8bef5bcf03d0
c-rew1nd <dreektseayger@gmail.com>
2024-10-22 11:47:00 +0530

    secured url using .env file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect")
@@ -15,7 +16,7 @@ app.use("/api/products", products_routes)
 
 const start  = async() => {
     try{
-        await connectDB();
+        await connectDB(process.env.MONGODB_URI);
         app.listen(PORT , () =>{
             console.log(`${PORT} Yes i am connected`)
         })
