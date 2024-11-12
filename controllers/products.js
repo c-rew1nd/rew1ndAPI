257322e8444d473a4c23e7221d2c9005892f144c
c-rew1nd <dreektseayger@gmail.com>
2024-11-12 16:12:35 +0530

    added Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -25,6 +25,13 @@ const getAllProducts = async (req, res) => {
       const fieldsToSelect = select.split(',').join(' ');
       productQuery = productQuery.select(fieldsToSelect);
     }
+
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+    let skip  = (page-1) *limit;
+
+    productQuery = productQuery.skip(skip).limit(limit)
     
     const myData = await productQuery;
     res.status(200).json({ myData });
